e8fb72c318185c36da0fb857b26119a2bcde0569
srivalli.cloud <vineetharajanala3@gmail.com>
2024-08-25 14:46:51 +0000

    function 4 code added

--- sample.java
+++ sample.java
@@ -5,4 +5,10 @@ public static void main(string args [])
 public function1()
 {	
 	system.out.println("function1 code added");
-}	
+}
+public function4()
+{
+	system.out.println("function4 code added");
+
+}		
+
